e5b84269257961febbb93c0d9345c96974a4881e
caicindy87 <caicindy87@gmail.com>
2020-04-23 13:27:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const body = document.querySelector('body')
+  const userData = {
+    name: userName,
+    email: userEmail
+  }
+
+  const postObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  }
+  
+  return fetch('http://localhost:3000/users', postObj).then(resp => resp.json()).then(
+    user => {
+    body.append(user.id)
+  }
+  ).catch(function(error) {
+    body.append(error.message)
+  })
+}
+
+submitData('jess', 'jess@example.com');
