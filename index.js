6e0a92cf7f59651bdfda75e71dfeaa9e3ebc3806
Edward Mendoza <lalo.garibo90@gmail.com>
2020-04-22 17:51:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name,
+    email,
+  };
+
+  let confObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  return fetch("http://localhost:3000/users", confObject)
+    .then((resp) => resp.json())
+    .then((user) => appendDataToDOM(user.id))
+    .catch((e) => renderError(e));
+}
+
+function appendDataToDOM(id) {
+  let h2 = document.createElement("h2");
+  h2.innerHTML = id;
+  document.body.appendChild(h2);
+}
+
+function renderError(error) {
+  let h3 = document.createElement("h3");
+  h3.innerHTML = error.message;
+  document.body.appendChild(h3);
+}
